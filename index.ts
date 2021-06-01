3ef13fb40b903bbf70e241d5838644ffc4f1f902
blen_meku <mekuriablen@gmail.com>
2021-06-01 13:50:02 -0700

    angular lesson one

---
+++ index.ts
@@ -0,0 +1,32 @@
+interface Person {
+  firstName: string;
+  lastName?: string;
+}
+
+interface Contact {
+  phoneNumber: number;
+  email?: string;
+}
+
+class ContactCard implements Person, Contact {
+  phoneNumber: number;
+  firstName: string;
+  constructor(firstName: string, phoneNumber: number) {
+    this.firstName = firstName;
+    this.phoneNumber = phoneNumber;
+  }
+  sendMessage(): void {
+    console.log(
+      "Name:" +
+        " " +
+        this.firstName +
+        ", " +
+        "Phone Number:" +
+        " " +
+        this.phoneNumber
+    );
+  }
+}
+
+let newPerson = new ContactCard("Henry", 1234567890);
+newPerson.sendMessage();
